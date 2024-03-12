38f24e263b1a61923164ae2b9149b6abb55eea7f
Adam Hillo <adam1hillo@gmail.com>
2024-03-12 10:38:33 +0100

    Inleiding

---
+++ src/main/java/be/vdab/theorie/Jaar.java
@@ -0,0 +1,19 @@
+package be.vdab.theorie;
+
+class Jaar {
+
+    private final int jaar;
+
+    Jaar(int jaar) {
+        this.jaar = jaar;
+    }
+
+    boolean isSchrikkelJaar() {
+        return jaar % 4 == 0 && jaar % 100 != 0 || jaar % 400 == 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(jaar);
+    }
+}
---
+++ src/test/java/be/vdab/theorie/JaarTest.java
@@ -0,0 +1,31 @@
+package be.vdab.theorie;
+
+import static org.assertj.core.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JaarTest {
+
+    @Test
+    void eenJaarDeelbaarDoor400IsEenSchrikkelJaar() {
+        Jaar jaar = new Jaar(2000);
+        assertThat(jaar.isSchrikkelJaar()).isTrue();
+    }
+    @Test
+    void eenJaarDeelbaarDoor100IsGeenSchrikkelJaar() {
+        assertThat(new Jaar(1900).isSchrikkelJaar()).isFalse();
+    }
+    @Test
+    void eenJaarDeelbaarDoor4isEenSchrikkeljaar() {
+        assertThat(new Jaar(2012).isSchrikkelJaar()).isTrue();
+    }
+    @Test
+    void eenJaarNietDeelbaarDoor4IsGeenSchrikkelJaar() {
+        assertThat(new Jaar(2015).isSchrikkelJaar()).isFalse();
+    }
+    @Test
+    void toStringgeeftHetJaarAlsTekst() {
+        assertThat(new Jaar(2015)).hasToString("2015");
+    }
+
+}
