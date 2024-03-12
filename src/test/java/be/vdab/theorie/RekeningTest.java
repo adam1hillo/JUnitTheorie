1b662674e22684b906bda56b0be68fb15beeec0b
Adam Hillo <adam1hillo@gmail.com>
2024-03-12 13:32:01 +0100

    @BeforeEach

--- src/test/java/be/vdab/theorie/RekeningTest.java
+++ src/test/java/be/vdab/theorie/RekeningTest.java
@@ -1,27 +1,31 @@
 package be.vdab.theorie;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class RekeningTest {
+    private Rekening rekening;
+
+    @BeforeEach
+    void beforeEach() {
+        rekening = new Rekening();
+    }
     @Test
     void hetSaldoVanEenNieuwRekeningIs0€() {
-        Rekening rekening = new Rekening();
         assertThat(rekening.getSaldo()).isZero();
     }
     @Test
     void nadatJe10€StortIsHetSaldo10€() {
-        Rekening rekening = new Rekening();
         rekening.stort(BigDecimal.valueOf(10));
         assertThat(rekening.getSaldo()).isEqualByComparingTo("10");
     }
     @Test
     void nadatJe10€En1€StortIsHetSaldo11€() {
-        Rekening rekening = new Rekening();
         rekening.stort(BigDecimal.TEN);
         rekening.stort(BigDecimal.ONE);
         assertThat(rekening.getSaldo()).isEqualByComparingTo("11");
