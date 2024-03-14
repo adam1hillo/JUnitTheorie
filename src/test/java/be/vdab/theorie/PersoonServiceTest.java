8281647fcd4b213f3fcd806d726f4d993e8f2f98
Adam Hillo <adam1hillo@gmail.com>
2024-03-14 15:43:53 +0100

    Mock

--- src/test/java/be/vdab/theorie/PersoonServiceTest.java
+++ src/test/java/be/vdab/theorie/PersoonServiceTest.java
@@ -2,19 +2,36 @@ package be.vdab.theorie;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class PersoonServiceTest {
+
+    @Mock
+    private PersoonRepository repository;
     private PersoonService service;
 
     @BeforeEach
     void beforeEach() {
-        service = new PersoonService(new PersoonRepositoryStub());
+        service = new PersoonService(repository);
     }
     @Test
     void deWeddeStandaardAfwijkingIs2() {
+        when(repository.findAllWeddes()).thenReturn(
+                Stream.of(2,4,4,4,5,5,7,9)
+                        .map(BigDecimal::valueOf)
+                        .toList());
         assertThat(service.standaardAfwijkingWeddes()).isEqualByComparingTo("2");
+        verify(repository).findAllWeddes();
     }
 }
